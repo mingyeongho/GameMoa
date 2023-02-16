75bd6a5a00a7e5f26478f5dab9d3d2b3ad9e434d
민경호 <1736s@naver.com>
2023-02-16 17:55:58 +0900

    feat: struct DavinciCode Container

---
+++ src/components/DavinciCode/DavinciCodeContainer.tsx
@@ -0,0 +1,7 @@
+import * as S from "./DavinciCodeContainer.style";
+
+const DavinciCodeContainer = () => {
+  return <S.DavinciCodeContainer>Davinci Code</S.DavinciCodeContainer>;
+};
+
+export default DavinciCodeContainer;
